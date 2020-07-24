95a3c9b02db56c27920b530640089d0f0b889117
Zhengyi <admin@mczhengyi.top>
2020-07-24 13:15:39 +0800

    添加按钮 新的,背景色,背景图 接口

--- src/components/VividBtn.vue
+++ src/components/VividBtn.vue
@@ -1,11 +1,14 @@
 <template>
 
     <v-hover v-slot:default="{hover}">
-        <div class="vivid-btn" :class="hover?'elevation-8':'elevation-3'" v-ripple>
-            <div class="vivid-btn-bg"></div>
-            <div class="vivid-btn-color"></div>
-            <div ref="VividBtnLinear" class="vivid-btn-color-linear"></div>
-            <div ref="VividBtnContent" class="vivid-btn-content"><slot></slot></div>
+        <div :style="{'--color': color}" class="vivid-btn-box">
+            <div class="vivid-btn-new" v-if="isNew">new</div>
+            <div class="vivid-btn" :class="hover?'elevation-8':'elevation-3'" v-ripple>
+                <div class="vivid-btn-bg" :style="{'background-image':  'url(' + bgImg + ')'}"></div>
+                <div class="vivid-btn-color"></div>
+                <div ref="VividBtnLinear" class="vivid-btn-color-linear"></div>
+                <div ref="VividBtnContent" class="vivid-btn-content"><slot></slot></div>
+            </div>
         </div>
     </v-hover>
 
@@ -23,11 +26,36 @@
                 this.$refs.VividBtnLinear.style.right = 20 * zoom + 10 + "px"
                 this.$refs.VividBtnContent.style.paddingRight = (40 * zoom) + 10 + "px";
             }
+        },
+        props: {
+            isNew: {
+                    type: Boolean,
+                    default() {
+                        return false
+                    }
+            },
+            bgImg: {
+                type: String,
+                default() {
+                    return require("../assets/test.png")
+                }
+            },
+            color: {
+                type: String,
+                default() {
+                    return "rgb(255, 218, 233)"
+                }
+            }
         }
     }
 </script>
 
 <style scoped>
+    .vivid-btn-box {
+        position: relative;
+        display: inline-block;
+    }
+
     .vivid-btn {
         display: inline-block;
         position: relative;
@@ -41,12 +69,26 @@
         transition: 0.2s;
     }
 
+    .vivid-btn-new {
+        position: absolute;
+        z-index: 10;
+        right: -5px;
+        padding: 0 6px;
+        font-size: 10px;
+        text-align: center;
+        color: white;
+        background-color: rgb(24, 102, 193);
+        border-radius: 20px;
+    }
+
     .vivid-btn-bg {
         position: absolute;
         z-index: -3;
         width: 100%;
         height: 100%;
-        background: url("../assets/test.png") no-repeat right;
+        background-image: url("../assets/test.png");
+        background-repeat: no-repeat;
+        background-position: right;
         background-size: contain;
     }
 
@@ -56,7 +98,7 @@
         z-index: -1;
         width: 45px;
         height: 100%;
-        background: linear-gradient(to right, rgb(255, 218, 233) 30%, transparent);
+        background: linear-gradient(to right, var(--color) 30%, transparent);
     }
 
     .vivid-btn-color {
@@ -64,7 +106,7 @@
         z-index: -4;
         width: 100%;
         height: 100%;
-        background-color: rgb(255, 218, 233);
+        background-color: var(--color);
     }
 
     .vivid-btn-content {
--- src/views/container/Container.vue
+++ src/views/container/Container.vue
@@ -4,8 +4,8 @@
 			<v-card style="margin: 8px auto;">
 				<v-card-title class="headline mb-1">分类1</v-card-title>
 				<v-card-text class="">
-					<vivid-btn>prprppr</vivid-btn>
-					<vivid-btn>prprpttttttttrprpr</vivid-btn>
+					<vivid-btn :is-new="true">prprppr</vivid-btn>
+					<vivid-btn :bg-img="'https://img.mczhengyi.top/webtest.png'">prprpttttttttrprpr</vivid-btn>
 					<vivid-btn>prprprprpr</vivid-btn>
 					<vivid-btn>prprpr</vivid-btn>
 					<vivid-btn>prprprprpr</vivid-btn>
@@ -13,11 +13,11 @@
 					<vivid-btn>prprprprpr</vivid-btn>
 					<vivid-btn>prprprpr</vivid-btn>
 					<vivid-btn>prprprprpr</vivid-btn>
-					<vivid-btn>prprprpdfgrpr</vivid-btn>
+					<vivid-btn :is-new="true">prprprpdfgrpr</vivid-btn>
 					<vivid-btn>prprprprpr</vivid-btn>
 					<vivid-btn>prpprrpr</vivid-btn>
 					<vivid-btn>prrpr</vivid-btn>
-					<vivid-btn>prdfgdrpr</vivid-btn>
+					<vivid-btn :is-new="true">prdfgdrpr</vivid-btn>
 					<vivid-btn>prprpdfgsdfsdfrprpr</vivid-btn>
 					<vivid-btn>prpprpr</vivid-btn>
 				</v-card-text>
